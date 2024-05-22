82eac61ae9f2f0c8e45fe3a65b9cd60909abf95a
Administrator <melbustazz@gmail.com>
2024-05-22 15:30:08 +0300

    inception

--- utils/dbms.py
+++ utils/dbms.py
@@ -13,7 +13,21 @@ def create_tables():
         first_name TEXT,
         date_reg TEXT
         )'''
-        #cursor.execute(create_table_users)
-        #cursor = conn.cursor()
-        #create_table
-        #create_table_queue =
+        create_table_masters = '''
+        CREATE TABLE IF NOT EXISTS masters(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        telephone TEXT,
+        tg_id TEXT,
+        earned TEXT)'''
+        create_table_services = '''
+        CREATE TABLE IF NOT EXISTS services(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        price TEXT,
+        master TEXT
+        )'''
+
+        cursor.execute(create_table_users)
+        cursor.execute(create_table_masters)
+        cursor.execute(create_table_services)
